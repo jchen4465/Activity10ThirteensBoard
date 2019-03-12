502bcebe6113cdd51690b21e99ec3d523bc819a6
BT_1N3_23 <jchen4465@bths.edu>
2019-03-12 08:31:36 -0400

    asassa

--- src/ElevensBoard.java
+++ src/ElevensBoard.java
@@ -27,7 +27,7 @@ public class ElevensBoard extends Board {
      * The values of the cards for this game to be sent to the deck.
      */
     private static final int[] POINT_VALUES =
-            {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0, 0, 0};
+            {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 0};
 
     /**
      * Flag used to control debugging print statements.
